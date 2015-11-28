479511928dd0263aa1ac5311ea8563778ebb2e57
Tomás <tomdagz@gmail.com>
2015-11-28 14:46:28 -0600

    added plugin to minify javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 
+var uglify = require('gulp-uglify');
+
 gulp.task('default', function() {
-  // place code for your default task here
+  gulp.src('js/*.js').pipe(uglify()).pipe(gulp.dest('minjs'));
 });
