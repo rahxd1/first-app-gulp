b51a56038ac9d224b1df2caeb866298fe66430cf
Tomás <tomdagz@gmail.com>
2015-11-28 15:22:39 -0600

    added plumber to keep gulp running

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ var uglify = require('gulp-uglify');
 
 var sass = require('gulp-ruby-sass');
 
+var plumber = require('gulp-plumber');
+
 // Tasks
 
 gulp.task('scripts', function(){
-	gulp.src('js/*.js').pipe(uglify()).pipe(gulp.dest('minjs'));	
+	gulp.src('js/*.js').pipe(plumber()).pipe(uglify()).pipe(gulp.dest('minjs'));	
 })
 
 gulp.task('styles', function(){
-	gulp.src('scss/**/*.scss').pipe(sass({sytle: 'compressed'})).pipe(gulp.dest('mincss/'));	
+	gulp.src('scss/**/*.scss').pipe(plumber()).pipe(sass({sytle: 'compressed'})).pipe(gulp.dest('mincss/'));	
 })
 
 gulp.task('watch', function(){
