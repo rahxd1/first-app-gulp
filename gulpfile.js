f24605666ba5137ea16dad6e04027050935a3050
Tomás <tomdagz@gmail.com>
2015-11-28 15:15:01 -0600

    added task to compile with sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,21 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 
+var sass = require('gulp-ruby-sass');
+
 // Tasks
 
 gulp.task('scripts', function(){
 	gulp.src('js/*.js').pipe(uglify()).pipe(gulp.dest('minjs'));	
 })
 
+gulp.task('styles', function(){
+	gulp.src('scss/**/*.scss').pipe(sass({sytle: 'compressed'})).pipe(gulp.dest('mincss/'));	
+})
+
 gulp.task('watch', function(){
 	gulp.watch('js/*.js', ['scripts'])
+	gulp.watch('scss/**/*.scss', ['styles'])
 });
 
 gulp.task('default', ['scripts', 'watch']);
